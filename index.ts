74c7463c8c5a7b86808c40d2e83f617913f63235
snomiao <snomiao@gmail.com>
2024-06-21 23:16:21 +0900

    feat(peek-json): init
    init

--- index.ts
+++ index.ts
@@ -16,10 +16,38 @@ export function peekLog<T extends Promise<any> | any>(
 
 export function peekYaml<T extends Promise<any> | any>(
   val: T,
+  field?: string | number,
   log = (val: any) => console.log(yaml.stringify(val).trim())
 ): T {
   (async function () {
-    if (val instanceof Promise) {
+    if (field !== undefined) {
+      if (val instanceof Promise) {
+        log({ [field]: await val });
+      } else {
+        log({ [field]: val });
+      }
+    } else if (val instanceof Promise) {
+      log(await val);
+    } else {
+      log(val);
+    }
+  })();
+  return val;
+}
+
+export function peekJSON<T extends Promise<any> | any>(
+  val: T,
+  field?: string | number,
+  log = (val: any) => console.log(JSON.stringify(val))
+): T {
+  (async function () {
+    if (field !== undefined) {
+      if (val instanceof Promise) {
+        log({ [field]: await val });
+      } else {
+        log({ [field]: val });
+      }
+    } else if (val instanceof Promise) {
       log(await val);
     } else {
       log(val);
